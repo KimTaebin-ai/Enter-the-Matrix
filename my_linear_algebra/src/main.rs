ecc2a2be7178134b561f490f5634970123993d94
Taebin Kim <76577776+KimTaebin-ai@users.noreply.github.com>
2026-03-04 16:26:02 +0900

    Solving ex00
    - Formatter 추가하여 출력문제 해결

--- my_linear_algebra/src/main.rs
+++ my_linear_algebra/src/main.rs
@@ -2,9 +2,20 @@
 // K 는 abstract algebra 로 봤을 때 scalar 의 체로 볼 수 있음.
 
 use std::ops::{AddAssign, SubAssign, MulAssign};
+use std::fmt;
 
 struct Vector<K> {
     data: Vec<K>,
+}
+
+impl<K> fmt::Display for Vector<K>
+where K: fmt::Display {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for val in &self.data {
+            writeln!(f, "[{:.1}]", val)?;
+        }
+        Ok(())
+    }
 }
 
 impl<K> Vector<K> 
@@ -33,9 +44,9 @@ where K: AddAssign + SubAssign + MulAssign + Copy {
 // }
 
 
-struct  Matrix<K> {
-    data: Vec<Vec<K>>,
-}
+// struct  Matrix<K> {
+//     data: Vec<Vec<K>>,
+// }
 
 
 
