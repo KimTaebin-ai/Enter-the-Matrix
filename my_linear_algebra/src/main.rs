34e9147d2c2a3e24658a59f28272cc0f28326458
Taebin Kim <76577776+KimTaebin-ai@users.noreply.github.com>
2026-03-04 17:55:25 +0900

    solving ex00
    - vector 로 add, sub, scalar multi 구현 완료

--- my_linear_algebra/src/main.rs
+++ my_linear_algebra/src/main.rs
@@ -1,6 +1,14 @@
 // K 라는 타입을 담을 건데, 이건 나중에 정하겠음
 // K 는 abstract algebra 로 봤을 때 scalar 의 체로 볼 수 있음.
 
+/**
+ * We recommend you to implement some utility functions, such as:
+• A function to return the size/shape of a vector/matrix.
+• A function to tell if a matrix is square.
+• A function to print a vector/matrix on the standard output.
+• A function to reshape a vector into a matrix, and vice-versa.
+ */
+
 use std::ops::{AddAssign, SubAssign, MulAssign};
 use std::fmt;
 
@@ -24,17 +32,30 @@ where K: AddAssign + SubAssign + MulAssign + Copy {
         Vector { data }
     }
 
-    // fn add(&mut self, v: &Vector<K>) {
-    
-    // }
+    pub fn add(&mut self, v: Vector<K>) {
+        if self.data.len() != v.data.len() {
+            panic!("Vector sizes must match for addition");
+        }
 
-    // fn sub(&mut self, v: &Vector<K>) {
+        for i in 0..self.data.len() {
+            self.data[i] += v.data[i];
+        }
+    }
+    pub fn sub(&mut self, v: Vector<K>) {
+        if self.data.len() != v.data.len() {
+            panic!("Vector sizes must match for subtraction");
+        }
 
-    // }
+        for i in 0..self.data.len() {
+            self.data[i] -= v.data[i];
+        }
+    }
 
-    // fn scl(&mut self, a: K) {
-
-    // }
+    pub fn scl(&mut self, a: K) {
+        for i in 0..self.data.len() {
+            self.data[i] *= a;
+        }
+    }
 }
 
 // impl<K> Matrix<K> {
@@ -53,19 +74,19 @@ where K: AddAssign + SubAssign + MulAssign + Copy {
 fn main() {
     let mut u = Vector::from(vec![2., 3.]);
     let v = Vector::from(vec![5., 7.]);
-    // u.add(v);
+    u.add(v);
     println!("{}", u);
     // [7.0]
     // [10.0]
-    // let mut u = Vector::from(vec![2., 3.]);
-    // let v = Vector::from(vec![5., 7.]);
-    // u.sub(v);
-    // println!("{}", u);
+    let mut u = Vector::from(vec![2., 3.]);
+    let v = Vector::from(vec![5., 7.]);
+    u.sub(v);
+    println!("{}", u);
     // // [-3.0]
     // // [-4.0]
-    // let mut u = Vector::from(vec![2., 3.]);
-    // u.scl(2.);
-    // println!("{}", u);
+    let mut u = Vector::from(vec![2., 3.]);
+    u.scl(2.);
+    println!("{}", u);
     // [4.0]
     // [6.0]
 }
