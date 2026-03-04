5edf31df245ab4e0789653f3a7207dcea2b96797
Taebin Kim <76577776+KimTaebin-ai@users.noreply.github.com>
2026-03-04 15:55:17 +0900

    solving ex00

---
+++ my_linear_algebra/src/main.rs
@@ -0,0 +1,60 @@
+// K 라는 타입을 담을 건데, 이건 나중에 정하겠음
+// K 는 abstract algebra 로 봤을 때 scalar 의 체로 볼 수 있음.
+
+use std::ops::{AddAssign, SubAssign, MulAssign};
+
+struct Vector<K> {
+    data: Vec<K>,
+}
+
+impl<K> Vector<K> 
+where K: AddAssign + SubAssign + MulAssign + Copy {
+    fn from(data: Vec<K>) -> Self {
+        Vector { data }
+    }
+
+    // fn add(&mut self, v: &Vector<K>) {
+    
+    // }
+
+    // fn sub(&mut self, v: &Vector<K>) {
+
+    // }
+
+    // fn scl(&mut self, a: K) {
+
+    // }
+}
+
+// impl<K> Matrix<K> {
+//     fn add(&mut self, v: &Matrix<K>);
+//     fn sub(&mut self, v: &Matrix<K>);
+//     fn scl(&mut self, a: K);
+// }
+
+
+struct  Matrix<K> {
+    data: Vec<Vec<K>>,
+}
+
+
+
+fn main() {
+    let mut u = Vector::from(vec![2., 3.]);
+    let v = Vector::from(vec![5., 7.]);
+    // u.add(v);
+    println!("{}", u);
+    // [7.0]
+    // [10.0]
+    // let mut u = Vector::from(vec![2., 3.]);
+    // let v = Vector::from(vec![5., 7.]);
+    // u.sub(v);
+    // println!("{}", u);
+    // // [-3.0]
+    // // [-4.0]
+    // let mut u = Vector::from(vec![2., 3.]);
+    // u.scl(2.);
+    // println!("{}", u);
+    // [4.0]
+    // [6.0]
+}
